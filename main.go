7346ff42c0e629e1080acf258c5136279527f4b5
Scott Allen <scott@plum.io>
2021-05-22 15:24:57 -0400

    Add basic routing and Content-Type

--- main.go
+++ main.go
@@ -6,7 +6,20 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>What's up?</h1>")
+	w.Header().Set("Content-Type", "text/html")
+
+	switch page := r.URL.Path; page {
+	case "/":
+		fmt.Fprint(w, "<h1>What's up?</h1>")
+	case "/contact":
+		fmt.Fprint(
+			w,
+			`To get in touch, please send an email to
+			<a href="mailto:foo@example.com">me</a>`,
+		)
+	default:
+		fmt.Fprint(w, "404")
+	}
 }
 
 func main() {
